e98847493e2bf08d84b7666d119e4c030a9e8f51
udianand <uditanand@ymail.com>
2017-01-24 11:29:24 -0600

    Create new branch for Bill

--- VaR0.001.R
+++ VaR0.001.R
@@ -19,10 +19,11 @@
 #UNEMP: Unemployment Rate Non-Seasonally Adjusted USA  - %
 
 CPI = CPI_All_items 
-FFR = FFR_Monthly
+FFR = FFR_Monthly #(Note: you might have to change the '_' to '-' depending upon your method
+                  #of loading data)
 UNEMP = Unemployment_rate
 
-#Data Cleaning
+##Data Cleaning
 #------------#
 #------------#
 
@@ -78,31 +79,56 @@ summary(UNEMP)
 #save.image(file="VaR0.0001.RData") Udit: Do not Uncomment
 ##*********************************************************##
 
-#Convert into a Time Series.
+##Convert into a Time Series.
+#---------------------------#
+
+#install.packages("quantmod")
+#install.packages("zoo")
+
 library(quantmod)
 library(zoo)
 
-UNEMP$TS = as.yearmon(as.character(UNEMP$Time), "%Y%m")
-UNEMP$TS = as.Date(UNEMP$TS)
+#UNEMP
+UNEMP$Time = as.yearmon(as.character(UNEMP$Time), "%Y%m")
+UNEMP$Time = as.Date(UNEMP$Time)
 head(UNEMP)
 
-UNEMP.xts<-xts(UNEMP$UnempRate, order.by=UNEMP$TS) 
+UNEMP.xts<-xts(UNEMP$UnempRate, order.by=UNEMP$Time) 
 chartSeries(UNEMP.xts)
-#*******#
 
-FFR$TS <- as.yearmon(as.character(FFR$Time), "%Y%m")
-FFR$TS = as.Date(FFR$TS)
+#FFR
+FFR$Time <- as.yearmon(as.character(FFR$Time), "%Y%m")
+FFR$Time = as.Date(FFR$Time)
 head(FFR)
 
 FFR.xts<-xts(FFR$InterestRates, order.by=FFR$TS) 
 chartSeries(FFR.xts)
-#******#
 
+#CPI
 CPI$TS = as.yearmon(as.character(CPI$Time), "%Y%m")
 CPI$TS = as.Date(CPI$TS)
 head(CPI)
 
 CPI.xts<-xts(CPI$CpiSeaAdj, order.by=CPI$TS) 
 chartSeries(CPI.xts)
+
+##Check for Stationarity
+#-----------------------#
+##Since a prereq for VAR is stationarity. We run the Dickie Fuller test for stationarity.
+
+#Perform the Dickie Fuller Test for Unit Roots
+adf.test()
+adf.test(y.DGS10.weekly)
+adf.test(y.DGS10.monthly)
+
+#Since the p-values do not fall below  the standard critical values 0.01 or 0.05
+#WE CANNOT REJECT THE NULL HYPOTHESIS
+
+#The p-value is the probability (assuming the null hypothesis is true) of realizing
+#a test statistic as extreme as that computed for the input series. Smaller
+#values (i.e., lower probabilities) provide stronger evidence against the null hyptohesis.
+#The p-value decreases as the periodicity of the data shortens. This suggests
+#that the time-series structure in the series DGS10 may be stronger at higher
+#frequencies.
 
 
