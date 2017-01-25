aa309cf77bc0314fa219eb9511f9dd5a20a386e7
udianand <uditanand@ymail.com>
2017-01-24 18:44:12 -0600

    VAR0.001.R core part is complete.

--- VaR0.001.R
+++ VaR0.001.R
@@ -1,7 +1,5 @@
 #VAR model - 0.001
 
-setwd("C:/Users/XingBill/Desktop/IAQFfolder/IAQF")
-
 #load workspace
 #load(file="VaR0.0001.RData") #Udit: Do Not Uncomment.
 
@@ -103,7 +101,7 @@ head(UNEMP)
 summary(UNEMP)
 
 #Crude Oil Data
-#--------#
+#-------------#
 Crude_Oil = Crude_Oil[-1, ] #The first row contained text varibales
 names(Crude_Oil)
 names(CPI)[2]<-"Price" #Change colnames
@@ -125,30 +123,167 @@ summary(Crude_Oil)
 ##*********************************************************##
 
 #Convert into a Time Series.
+#-------------------------#
+#--------------------------#
 library(quantmod)
 library(zoo)
 
-UNEMP$TS = as.yearmon(as.character(UNEMP$Time), "%Y%m")
-UNEMP$TS = as.Date(UNEMP$TS)
+#UNEMP#
+#------#
+UNEMP$Time = as.yearmon(as.character(UNEMP$Time), "%Y%m")
+UNEMP$Time = as.Date(UNEMP$Time)
 head(UNEMP)
 
-UNEMP.xts<-xts(UNEMP$UnempRate, order.by=UNEMP$TS) 
+UNEMP.xts<-xts(UNEMP$UnempRate, order.by=UNEMP$Time) 
 chartSeries(UNEMP.xts)
-#*******#
 
-FFR$TS <- as.yearmon(as.character(FFR$Time), "%Y%m")
-FFR$TS = as.Date(FFR$TS)
+#FFR#
+#----#
+FFR$Time <- as.yearmon(as.character(FFR$Time), "%Y%m")
+FFR$Time = as.Date(FFR$Time)
 head(FFR)
 
-FFR.xts<-xts(FFR$InterestRates, order.by=FFR$TS) 
+FFR.xts<-xts(FFR$InterestRates, order.by=FFR$Time) 
 chartSeries(FFR.xts)
-#******#
 
-CPI$TS = as.yearmon(as.character(CPI$Time), "%Y%m")
-CPI$TS = as.Date(CPI$TS)
+#CPI
+#---#
+CPI$Time = as.yearmon(as.character(CPI$Time), "%Y%m")
+CPI$Time = as.Date(CPI$Time)
 head(CPI)
 
-CPI.xts<-xts(CPI$CpiSeaAdj, order.by=CPI$TS) 
+CPI.xts<-xts(CPI$CpiSeaAdj, order.by=CPI$Time) 
 chartSeries(CPI.xts)
 
 
+#source("DataCleaning.R")
+
+##Stationarity Test
+#*******************#
+#*******************#
+library(tseries)
+
+dim(CPI.xts)
+dim(FFR.xts)
+dim(UNEMP.xts)
+
+#Perform the Dickie Fuller Test for Unit Roots
+adf.test(CPI.xts) #p-value = 0.546 (CANNOT REJECT NULL HYPOTHESIS)
+adf.test(FFR.xts) #p-value = 0.01708 (CANNOT NULL HYPOTHESIS)
+adf.test(UNEMP.xts) #p-value = 0.8873 (CANNOT REJECT NULL HYPOTHESIS)
+
+
+##Difference the time series
+adf.test(na.omit(diff(CPI.xts))) #p-value < 0.01 (REJECT NULL HYPOTHESIS)
+adf.test(na.omit(diff(UNEMP.xts))) #p-value < 0.01 (REJECT NULL HYPOTHESIS)
+
+
+CPI.xts.diff = na.omit(diff(CPI.xts))
+UNEMP.xts.diff = na.omit(diff(UNEMP.xts))
+
+#Difference twice
+FFR.xts.diff  = na.omit(diff(FFR.xts))
+FFR.xts.diff  = na.omit(diff(FFR.xts.diff))
+
+##Plot the ACF and PACF
+par(mfcol=c(2,1))
+acf(CPI.xts.diff)
+acf(CPI.xts.diff,type="partial")
+
+par(mfcol=c(2,1))
+acf(FFR.xts.diff)
+acf(FFR.xts.diff,type="partial")
+
+par(mfcol=c(2,1))
+acf(UNEMP.xts.diff)
+acf(UNEMP.xts.diff,type="partial")
+
+##VAR MODEL
+#----------#
+#-----------#
+library(vars)
+
+#Consider focusing on 3 variables
+varmod.001 = merge(UNEMP.xts.diff, FFR.xts.diff, CPI.xts.diff)
+
+varmod.001 = na.omit(varmod.001)
+#TODO: Check how to deal with 'NA' Data
+#Currently, only two NA values in all of the data so we can omit
+
+#Auto-Select the VAR Model, Ignoring the NA values
+varmod.001.VAR.const<-VARselect(varmod.001, lag.max=12, type="const")
+
+#Print out the VAR order identified by different information criteria
+varmod.001.VAR.const$selection
+
+#Fit the VAR model corresponding to the Schwarz Criterion (SC) which is the BIC
+varmod.001.VAR.const.0 = VAR(varmod.001, p=varmod.001.VAR.const$selection[3],type="const")
+options(show.signif.stars=FALSE)
+summary(varmod.001.VAR.const.0)
+
+##Plot the Impulse Function
+#--------------------------#
+#--------------------------#
+
+#When FFR rises:
+plot(irf(varmod.001.VAR.const.0, impulse="FFR.xts.diff"))
+
+
+#QUARTERLY DATA#
+#--------------#
+#--------------#
+
+##Convert to Quarterly data and taking the closing prices
+CPI.xts.q = to.quarterly(CPI.xts)
+CPI.xts.q = CPI.xts.q$CPI.xts.Close
+
+FFR.xts.q = to.quarterly(FFR.xts)
+FFR.xts.q = FFR.xts.q$FFR.xts.Close
+
+UNEMP.xts.q = to.quarterly(UNEMP.xts)
+UNEMP.xts.q = UNEMP.xts.q$UNEMP.xts.Close
+
+##Check for Stationarity
+adf.test(CPI.xts.q)
+adf.test(UNEMP.xts.q)
+adf.test(FFR.xts.q)
+
+##Difference the time series
+adf.test(na.omit(diff(CPI.xts.q))) #p-value < 0.01 (REJECT NULL HYPOTHESIS)
+adf.test(na.omit(diff(UNEMP.xts.q))) #p-value < 0.01 (REJECT NULL HYPOTHESIS)
+
+CPI.xts.q.diff = na.omit(diff(CPI.xts.q))
+UNEMP.xts.q.diff = na.omit(diff(UNEMP.xts.q))
+
+#Difference UNEMP Twice
+adf.test(na.omit(diff(UNEMP.xts.q.diff)))
+UNEMP.xts.q.diff = na.omit(diff(UNEMP.xts.q))
+
+##VAR MODEL FOR QUARTERLY
+#----------#
+#-----------#
+library(vars)
+
+#Consider focusing on 3 variables
+varmod.001.q = merge(UNEMP.xts.q.diff, FFR.xts.q, CPI.xts.q.diff)
+
+varmod.001.q = na.omit(varmod.001.q)
+#TODO: Check how to deal with 'NA' Data
+
+#Auto-Select the VAR Model, Ignoring the NA values
+varmod.001.q.VAR.const<-VARselect(varmod.001.q, lag.max=12, type="const")
+
+#Print out the VAR order identified by different information criteria
+varmod.001.q.VAR.const$selection
+
+#Fit the VAR model corresponding to the Schwarz Criterion (SC) which is the BIC
+varmod.001.q.VAR.const.0 = VAR(varmod.001.q, p=varmod.001.q.VAR.const$selection[3],type="const")
+options(show.signif.stars=FALSE)
+summary(varmod.001.q.VAR.const.0)
+
+##Plot the Impulse Function
+#--------------------------#
+#--------------------------#
+
+#When FFR rises:
+plot(irf(varmod.001.q.VAR.const.0, impulse="FFR.xts.Close"))
